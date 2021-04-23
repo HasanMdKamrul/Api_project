d90987dc3ea16ea058f58565aa203a64bf878c27
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2021-04-23 12:26:31 +0200

    learned few generics api views

--- core/views.py
+++ core/views.py
@@ -5,22 +5,49 @@ from .models import Post
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
 
-
-class PostApiView(APIView):
-
-    permission_classes = [IsAuthenticated]
-
-    def post(self,request,*args,**kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+class PostApiView(mixins.CreateModelMixin,mixins.ListModelMixin,generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
     def get(self,request,*args,**kwargs):
-        qs = Post.objects.all()
-        post = qs.first()
+        return self.list(request, *args, **kwargs)
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request, *args, **kwargs)
+
+class PostCreateView(mixins.ListModelMixin,generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
+class PostListCreateApiView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
+
+
+
+#class PostApiView(APIView):
+
+ #   permission_classes = [IsAuthenticated]
+
+  #  def post(self,request,*args,**kwargs):
+   #     serializer = PostSerializer(data=request.data)
+    #    if serializer.is_valid():
+     #       serializer.save()
+      #      return Response(serializer.data)
+       # return Response(serializer.errors)
+
+    #def get(self,request,*args,**kwargs):
+     #   qs = Post.objects.all()
+      #  post = qs.first()
         #serializer = PostSerializer(qs, many=True)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+       # serializer = PostSerializer(post)
+        #return Response(serializer.data)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from core.views import PostApiView
+from core.views import PostApiView,PostCreateView,PostListCreateApiView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('',PostApiView.as_view(), name="login"),
+    path('create/',PostCreateView.as_view(), name="create"),
+    path('create-api/',PostListCreateApiView.as_view(), name="create-api"),
     path('api/token/',obtain_auth_token, name='obtain-token')
 ]
